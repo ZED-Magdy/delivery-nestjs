74005b4e149ef4062b293f9d19053070a85e9e22
ZED-Magdy <zedmagdy@gmail.com>
2024-10-28 21:57:10 +0300

    create user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [DatabaseModule, ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    DatabaseModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    UserModule,
+  ],
   controllers: [],
-  providers: [],
 })
 export class AppModule {}
